f16847e3f3aba1475484506fa2c631b5d5446e74
Kevin <khampkevin@gmail.com>
2023-05-12 21:46:03 -0400

    Fixed JOIN and works properly in query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,1 @@
--- SELECT * FROM role JOIN department ON role.department_id = department.id
-
--- SELECT * FROM employee JOIN role ON employee.role_id = role.id
-
-SELECT employee.role_id FROM employee INNER JOIN role
-ON role.id = employee.role_id
+SELECT * FROM employee JOIN roles ON roles.id = employee.role_id;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const inquirer = require('inquirer')
 
 inquirer.prompt([
